03e4036e84ad13ba446a55ba76840e0ff420daa9
cnorma <cnorma@student.21-school.ru>
2022-01-20 22:59:56 +0300

    add src/main.c

--- src/main.c
+++ src/main.c
@@ -5,8 +5,26 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cnorma <cnorma@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/18 20:10:10 by cnorma            #+#    #+#             */
-/*   Updated: 2022/01/18 20:10:11 by cnorma           ###   ########.fr       */
+/*   Created: 2022/01/20 22:50:02 by cnorma            #+#    #+#             */
+/*   Updated: 2022/01/20 22:54:57 by cnorma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
+
+int	check_arg(int argc, char **arg)
+{
+	if (argc < 1)
+		return (-1);
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	(void)argv;
+
+	if (check_arg(argc, argv) != 0)
+		return (printf("Wrong arguments\n"));
+	printf ("Hello Minishell>\n");
+	return (0);
+}
