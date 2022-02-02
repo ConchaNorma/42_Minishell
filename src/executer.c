406e51a35dfffe3e95cccf46ae9ead914f7b7977
Kirill Yuzeev <aarnell@student.21-school.ru>
2022-02-02 22:21:48 +0300

    Added nothing

--- src/executer.c
+++ src/executer.c
@@ -6,13 +6,22 @@
 /*   By: aarnell <aarnell@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 19:18:59 by aarnell           #+#    #+#             */
-/*   Updated: 2022/01/23 19:57:38 by aarnell          ###   ########.fr       */
+/*   Updated: 2022/02/02 22:20:52 by aarnell          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static int pipe_and_fork(void)
+{
+	return (0);
+}
+
 int executer(void)
 {
+	char *str[] = {"ls", "-al", "|", "wc", "-l", NULL};
+
+
+
 	return (0);
 }
