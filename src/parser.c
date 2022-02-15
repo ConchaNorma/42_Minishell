f74edd0ce580d7bf504a879d99439a94ab14a6e3
cnorma <cnorma@student.21-school.ru>
2022-02-15 08:34:48 +0300

    add parsing of \> ot parser.c

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: cnorma <cnorma@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 18:30:14 by aarnell           #+#    #+#             */
-/*   Updated: 2022/02/10 22:29:17 by cnorma           ###   ########.fr       */
+/*   Updated: 2022/02/14 08:58:44 by cnorma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -177,6 +177,95 @@ static void ft_split_pipes(t_exec *vars)
 	free (tmp);
 }
 */
+
+char *ft_file_parser(char *str, int *i)
+{
+	int		j;
+	char	*tmp;
+
+	if (str[*i] == '>' || str[*i] == '<')
+		++(*i);
+	while (str[*i] == ' ')
+			++(*i);
+	j = *i;
+	while (ft_isalnum(str[*i]) || str[*i] == '[' || str[*i] == ']'\
+		|| str[*i] == '{' || str[*i] == '}' || str[*i] == '%' || str[*i] == '@'\
+		|| str[*i] == '!' || str[*i] == '.' || str[*i] == '~' || str[*i] == '='\
+		|| str[*i] == '+' || str[*i] == '-' || str[*i] == '_' || str[*i] == '#'\
+		|| str[*i] == '^')
+		++(*i);
+	tmp = NULL;
+	tmp = ft_substr(str, j, *i - j);
+	return (tmp);
+}
+
+t_redir	*ft_create_redir(void)
+{
+	t_redir	*tmp;
+
+	tmp = NULL;
+	tmp = (t_redir *)malloc(sizeof(t_redir));
+	if (!tmp)
+		exit (1);
+	//tmp->type = NULL;
+	tmp->fd = -1;
+	tmp->file = NULL;
+	tmp->next = NULL;
+	return (tmp);
+}
+
+t_redir	*ft_redir_sup(t_cmd *tmp_cmds)
+{
+	t_redir	*tmp_redir;
+	t_redir	*new;
+
+	if (!tmp_cmds->v_rdr){
+		tmp_cmds->v_rdr = ft_create_redir();
+		tmp_redir = tmp_cmds->v_rdr;
+	}
+	else{
+		tmp_redir = tmp_cmds->v_rdr;
+		new = ft_create_redir();
+		while (tmp_redir->next)
+			tmp_redir = tmp_redir->next;
+		tmp_redir->next = new;
+	}
+	while (tmp_redir->next)
+		tmp_redir = tmp_redir->next;
+	return (tmp_redir);
+}
+
+char	*ft_forward_redir(t_exec *vars, int *i)
+{
+	int		j;
+	t_cmd	*tmp_cmds;
+	t_redir	*tmp_redir;
+	char	*tmp;
+
+	tmp_cmds = vars->cmds;
+	while (tmp_cmds->next)
+		tmp_cmds = tmp_cmds->next;
+	tmp_redir = ft_redir_sup(tmp_cmds);
+	tmp_redir->type = OUT;
+	if (vars->str[++(*i)] == '>')
+		tmp_redir->type = APN;
+	j = *i;
+	tmp_redir->file = ft_file_parser(vars->str, &j);
+	tmp = ft_strjoin(ft_substr(vars->str, 0, *i - 1), ft_substr(vars->str, j, ft_strlen(vars->str) - j));
+	printf("3\n");
+	//printf("redir_tmp2= %s\n", tmp2);
+	printf("tmp_redir->type= %u\n", tmp_redir->type);
+	printf("tmp_redir->file= %s\n", tmp_redir->file);
+	printf("tmp= %s\n", tmp);
+	//printf("str_2= %s\n", tmp);
+	//tmp = ft_substr(str, 0, j);
+	//tmp2 = ft_substr(str, j + 1, *i - j - 1);
+	//tmp3 = ft_substr(str, *i + 1, ft_strlen(str) - *i);
+	//tmp = ft_strjoin(ft_strjoin(tmp, tmp2), tmp3);
+	//--(*i);
+	return (tmp);
+}
+
 t_cmd	*ft_create_cmds(void)
 {
 	t_cmd	*tmp;
@@ -195,24 +284,31 @@ char *ft_split_pipe(t_exec *vars, int *i)
 {
 	char	*tmp;
 	t_cmd	*new;
+	t_cmd	*tmp_cmds;
+	int		len;
 
-	if (vars->str[*i + 1] == '|')
-		exit(1);
-	new = ft_create_cmds();
-	if (vars->cmds)
-	{
-		while (vars->cmds)
-			vars->cmds = vars->cmds->next;
-		vars->cmds = new;
-	}
-	else
-		vars->cmds = new;
-	vars->cmds->cmd = ft_substr(vars->str, 0, *i);
+	len = *i;
+	tmp_cmds = vars->cmds;
+	//if (vars->str[*i + 1] == '|')
+	//	exit(1);
+	while (tmp_cmds->next)
+		tmp_cmds = tmp_cmds->next;
+	while (vars->str[len] == ' ')
+			len--;
+	tmp_cmds->cmd = ft_substr(vars->str, 0, len);
 	tmp = ft_substr(vars->str, *i + 1, ft_strlen(vars->str) - *i);
+	if (vars->str[*i])
+	{
+		new = ft_create_cmds();
+		tmp_cmds->next = new;
+	}
+
+	printf("2\n");
 	printf("str_0= %s\n", vars->str);
 	printf("str_2= %s\n", tmp);
 	printf("str_cmd= %s\n", vars->cmds->cmd);
-	*i = 0;
+	//vars->cmds = tmp_cmds;
+	*i = -1;
 	vars->st++;
 	return (tmp);
 }
@@ -220,8 +316,9 @@ char *ft_split_pipe(t_exec *vars, int *i)
 int parser(t_exec *vars)
 {
 	int	i;
+	t_cmd	*tmp_cmds;
 
-	vars->cmds = NULL;
+	vars->cmds = ft_create_cmds();
 	i = -1;
 	while (vars->str[++i])
 	{
@@ -235,23 +332,24 @@ int parser(t_exec *vars)
 			vars->str = ft_dollar(vars->str, &i, vars->envp);
 		if (vars->str[i] == ' ')
 			vars->str = ft_space(vars->str, &i);
-		if (vars->str[i] == '|' || !vars->str[i])
-			vars->str = ft_split_pipe(vars, &i);
 		if (vars->str[i] == '>')
-			continue ;
+			vars->str = ft_forward_redir(vars, &i);
 		if (vars->str[i] == '<')
 			continue ;
-		if (vars->str[i] == '$')
-			continue ;
+		if (vars->str[i] == '|')
+			vars->str = ft_split_pipe(vars, &i);
 	}
+	vars->str = ft_split_pipe(vars, &i);
 	printf("str_res= %s\n", vars->str);
+
 	//vars->cmds = ft_split_pipes(vars->str);
 	//ft_split_pipes(vars);
 	i = -1;
-	while (vars->cmds)
+	tmp_cmds = vars->cmds;
+	while (tmp_cmds)
 	{
-		printf("pipes[%d]= %s\n", ++i, vars->cmds->cmd);
-		vars->cmds = vars->cmds->next;
+		printf("pipes[%d]= %s\n", ++i, tmp_cmds->cmd);
+		tmp_cmds = tmp_cmds->next;
 	}
 
 	return (0);
