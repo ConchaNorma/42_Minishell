e0a3d61d681a84382bc32b0e9966305770b14678
Kirill Yuzeev <aarnell@student.21-school.ru>
2022-02-20 20:39:57 +0300

    feat: added func. builtin-unset

--- src/builtin_unset.c
+++ src/builtin_unset.c
@@ -6,7 +6,7 @@
 /*   By: aarnell <aarnell@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/19 15:22:05 by aarnell           #+#    #+#             */
-/*   Updated: 2022/02/20 20:15:31 by aarnell          ###   ########.fr       */
+/*   Updated: 2022/02/20 20:39:25 by aarnell          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,16 @@
 
 int	builtin_unset(t_exec *vars, char *var)
 {
-	char *t_str;
-	char *d_str;
-	int cnt;
+	int		pos;
+	char	**res;
 
-
-	if(!t_str)
+	pos = srch_var_in_envp(vars->envp, var);
+	if (pos == -1)
 		return (-1);
-	return (0);
+	res = ft_del_str_from_arr(vars->envp, vars->envp[pos]);
+	if (!res)
+		return (-1);
+	ft_frmtrx(vars->envp);
+	vars->envp = res;
+	return (pos);
 }
