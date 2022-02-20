ef4146fca30b5f56ddb3d238c3bf2fd21f0df546
cnorma <cnorma@student.21-school.ru>
2022-02-20 21:22:58 +0300

    deleted tmp = ft_strdup(vars->str); from ft_space in parser.c

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: cnorma <cnorma@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 18:30:14 by aarnell           #+#    #+#             */
-/*   Updated: 2022/02/20 18:14:45 by cnorma           ###   ########.fr       */
+/*   Updated: 2022/02/20 19:44:33 by cnorma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -110,7 +110,7 @@ char	*ft_space(t_exec *vars, int *i)
 	char	*tmp;
 
 	j = *i;
-	tmp = ft_strdup(vars->str);
+	//tmp = ft_strdup(vars->str);
 	if (*i == 0)
 	{
 		while (vars->str[++j] == ' ')
