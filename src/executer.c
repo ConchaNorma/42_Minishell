3dd886266d5175dfe131363a8df9dbcc44e51829
Kirill Yuzeev <aarnell@student.21-school.ru>
2022-02-22 21:21:16 +0300

    fix: added cond. check. builtins or execve-func

--- src/executer.c
+++ src/executer.c
@@ -6,7 +6,7 @@
 /*   By: aarnell <aarnell@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 19:18:59 by aarnell           #+#    #+#             */
-/*   Updated: 2022/02/21 22:44:19 by aarnell          ###   ########.fr       */
+/*   Updated: 2022/02/22 21:19:08 by aarnell          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,55 +20,37 @@
 static int call_parent(t_exec *vars)
 {
 	t_cmd *tmp;
-
-	// int i;
-
-	// i = -1;
-	// while (vars->cmds->cmd[++i])
-	// 	printf("vars->cmds->cmd[%d] = %s\n", i, vars->cmds->cmd[i]);
+	int res;
 
 	tmp = vars->cmds;
-	//vars->st--;
 	while (--vars->st)
 		tmp = tmp->next;
 	//здесь сделать обработку редиректов
-	redirection_fd(tmp->v_rdr);
-
-	// printf("len0 = %zu\n", ft_arrlen((void **)tmp->cmd));
-	// tmp->cmd = ft_add_str_to_arr(tmp->cmd, "");
-	// i = ft_arrlen((void **)tmp->cmd);
-	// printf("len1 = %d\n", i);
-	// tmp->cmd[i - 1] = NULL;
-	// printf("len2 = %zu\n", ft_arrlen((void **)tmp->cmd));
+	redirection_fd(tmp->v_rdr); //дописать обработку ошибок
 
 	//здесь сделать проверку на built-in и их выполнение. В случае, если это не они, выполнять то, что ниже
-	builtin_check(tmp->cmd, vars);
-	vars->path = get_path(vars->envp, tmp->cmd[0]);
-	if (!vars->path)
+	res = builtin_check(tmp->cmd, vars); 	//дописать обработку ошибок
+	if (!res)
 	{
-		ft_frmtrx(tmp->cmd);		//как сделать очистку в некоторой общей структуре
-		//сделать очистку списков и замолоченных структур
-		//здесь подумать на счет выхода
-		ft_exit(0, "The path to execute the parent command was not found.");
-	}
-	// printf("vars->path: %s\n", vars->path);
-	// i = -1;
-	// while (tmp->cmd[++i])
-	// 	printf("tmp->cmd[%d] = %s\n", i, tmp->cmd[i]);
-
-	//
-	//if (ft_exec(vars, tmp) == -1)
-	//if (execve(vars->path, cm, vars->envp) == -1)
-	if (execve(vars->path, tmp->cmd, vars->envp) == -1)
-	{
-		// printf("%s: err\n", tmp->cmd[0]);
+		vars->path = get_path(vars->envp, tmp->cmd[0]);
+		if (!vars->path)
+		{
+			ft_frmtrx(tmp->cmd);		//как сделать очистку в некоторой общей структуре
+			//сделать очистку списков и замолоченных структур
+			//здесь подумать на счет выхода
+			ft_exit(0, "The path to execute the parent command was not found.");
+		}
+		if (execve(vars->path, tmp->cmd, vars->envp) == -1)
+		{
+			// printf("%s: err\n", tmp->cmd[0]);
+			free(vars->path);
+			ft_frmtrx(tmp->cmd);
+			//сделать очистку списков и замолоченных структур
+			//здесь подумать на счет выхода
+			ft_exit(errno, NULL);
+		}
 		free(vars->path);
-		ft_frmtrx(tmp->cmd);
-		//сделать очистку списков и замолоченных структур
-		//здесь подумать на счет выхода
-		ft_exit(errno, NULL);
 	}
-	free(vars->path);
 	ft_frmtrx(tmp->cmd);
 	//сделать очистку списков и замолоченных структур
 	//закрыть и удалить временнй файл heredoc
