18f6c4f253ad4aa46c1f037fc3b52948bdf3ebcd
Kirill Yuzeev <aarnell@student.21-school.ru>
2022-02-22 19:55:49 +0300

    fix: added loop in builtin_unset

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: aarnell <aarnell@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 22:45:20 by cnorma            #+#    #+#             */
-/*   Updated: 2022/02/22 19:43:49 by aarnell          ###   ########.fr       */
+/*   Updated: 2022/02/22 19:51:14 by aarnell          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,7 +90,7 @@ int		srch_var_in_envp(char **envp, char *var_name);
 
 int		builtin_check(char **cmd, t_exec *vars);
 int		builtin_export(t_exec *vars, char **cmd);
-int		builtin_unset(t_exec *vars, char *var);
+int		builtin_unset(t_exec *vars, char **cmd);
 int		builtin_pwd(void);
 int		builtin_cd(char *dir, char **envp);
 int		builtin_env(char **envp);
--- src/builtin_unset.c
+++ src/builtin_unset.c
@@ -6,24 +6,37 @@
 /*   By: aarnell <aarnell@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/19 15:22:05 by aarnell           #+#    #+#             */
-/*   Updated: 2022/02/22 19:47:46 by aarnell          ###   ########.fr       */
+/*   Updated: 2022/02/22 19:52:25 by aarnell          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-int	builtin_unset(t_exec *vars, char *var)
+int	builtin_unset(t_exec *vars, char **cmd)
 {
 	int		pos;
 	char	**res;
+	int		i;
+	int		s;
 
-	pos = srch_var_in_envp(vars->envp, var);
-	if (pos == -1)
-		return (-1);
-	res = ft_del_str_from_arr(vars->envp, vars->envp[pos]);
-	if (!res)
-		return (-1);
-	ft_frmtrx(vars->envp);
-	vars->envp = res;
+	i = 0;
+	s = 0;
+	while(cmd[i])
+	{
+		if (s == 0 && !ft_memcmp(cmd[i], "unset", 6))
+			s = 1;
+		else if (s == 1)
+		{
+			pos = srch_var_in_envp(vars->envp, cmd[i]);
+			if (pos == -1)
+				return (-1);
+			res = ft_del_str_from_arr(vars->envp, vars->envp[pos]);
+			if (!res)
+				return (-1);
+			ft_frmtrx(vars->envp);
+			vars->envp = res;
+		}
+		i++;
+	}
 	return (1);
 }
