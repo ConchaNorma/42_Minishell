9f4d6143b06ae0236aab3db1f14a29c981023d49
Kirill Yuzeev <aarnell@student.21-school.ru>
2022-02-22 19:59:19 +0300

    fix: del. strings for check cmd in blt-unset

--- src/builtin_unset.c
+++ src/builtin_unset.c
@@ -6,7 +6,7 @@
 /*   By: aarnell <aarnell@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/19 15:22:05 by aarnell           #+#    #+#             */
-/*   Updated: 2022/02/22 19:52:25 by aarnell          ###   ########.fr       */
+/*   Updated: 2022/02/22 19:57:42 by aarnell          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,18 @@ int	builtin_unset(t_exec *vars, char **cmd)
 	int		pos;
 	char	**res;
 	int		i;
-	int		s;
 
-	i = 0;
-	s = 0;
+	i = 1;
 	while(cmd[i])
 	{
-		if (s == 0 && !ft_memcmp(cmd[i], "unset", 6))
-			s = 1;
-		else if (s == 1)
-		{
-			pos = srch_var_in_envp(vars->envp, cmd[i]);
-			if (pos == -1)
-				return (-1);
-			res = ft_del_str_from_arr(vars->envp, vars->envp[pos]);
-			if (!res)
-				return (-1);
-			ft_frmtrx(vars->envp);
-			vars->envp = res;
-		}
+		pos = srch_var_in_envp(vars->envp, cmd[i]);
+		if (pos == -1)
+			return (-1);
+		res = ft_del_str_from_arr(vars->envp, vars->envp[pos]);
+		if (!res)
+			return (-1);
+		ft_frmtrx(vars->envp);
+		vars->envp = res;
 		i++;
 	}
 	return (1);
