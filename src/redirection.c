0406865a5a74b6913b325060fd947d7b2a24a6f1
Kirill Yuzeev <aarnell@student.21-school.ru>
2022-03-20 18:36:12 +0300

    fix: merged inp-out-apn-redirect-func. into one

--- src/redirection.c
+++ src/redirection.c
@@ -6,44 +6,32 @@
 /*   By: aarnell <aarnell@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 19:28:34 by aarnell           #+#    #+#             */
-/*   Updated: 2022/03/17 22:21:02 by aarnell          ###   ########.fr       */
+/*   Updated: 2022/03/20 18:32:16 by aarnell          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-static int redir_out(t_redir *v_rdr)
+static int redir_inp_out_apn(t_redir *v_rdr)
 {
 	int fd;
+	int std;
 
-	fd = open(v_rdr->file, O_RDWR | O_CREAT | O_TRUNC, S_IWRITE | S_IREAD);
+	std = 1;
+	if (v_rdr->type == INP)
+	{
+		fd = open(v_rdr->file, O_RDONLY, S_IREAD);		//дописать: при открытии нового, закрывать старый
+		std = 0;
+	}
+	else if (v_rdr->type == OUT)
+		fd = open(v_rdr->file, O_RDWR | O_CREAT | O_TRUNC, S_IWRITE | S_IREAD);
+	else if (v_rdr->type == APN)
+		fd = open(v_rdr->file, O_RDWR | O_CREAT | O_APPEND, S_IWRITE | S_IREAD);
 	if (fd == -1)
 		return (-1);
-	dup2(fd, 1);
+	dup2(fd, std);
 	return (0);
 	//как быть при перенаправлении на один STDIN/STDOUT/STDERR
-}
-
-static int redir_inp(t_redir *v_rdr)
-{
-	int fd;
-	//дописать: при открытии нового, закрывать старый
-	fd = open(v_rdr->file, O_RDONLY, S_IREAD);
-	if (fd == -1)
-		return (-1);
-	dup2(fd, 0);
-	return (0);
-}
-
-static int redir_apn(t_redir *v_rdr)
-{
-	int fd;
-
-	fd = open(v_rdr->file, O_RDWR | O_CREAT | O_APPEND, S_IWRITE | S_IREAD);
-	if (fd == -1)
-		return (-1);
-	dup2(fd, 1);
-	return (0);
 }
 
 static int redir_heredoc(t_redir *v_rdr)
@@ -93,15 +81,12 @@ int redirection_fd(t_redir *v_rdr)
 	//дописать в редиректах открытие файла по пути типа /--/--/filename
 	while (v_rdr)
 	{
-		if (v_rdr->type == OUT && redir_out(v_rdr) == -1)
-			return (-1);
-		else if (v_rdr->type == INP && redir_inp(v_rdr) == -1)
-			return (-1);
-		else if (v_rdr->type == APN && redir_apn(v_rdr) == -1)
+		if ((v_rdr->type == INP || v_rdr->type == OUT || \
+			v_rdr->type == APN) && redir_inp_out_apn(v_rdr) == -1)
 			return (-1);
 		else if (v_rdr->type == HRD && redir_heredoc(v_rdr) == -1)
 			return (-1);
 		v_rdr = v_rdr->next;
 	}
-	return (0);	//возможно где-то здесь нужен обработчик ошибок
+	return (0);
 }
