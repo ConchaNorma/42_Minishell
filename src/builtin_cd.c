e194013d794fcfaf02e1e49ba6027bae37df8dff
Kirill Yuzeev <aarnell@student.21-school.ru>
2022-04-02 21:38:00 +0300

    fix: builtin_cd

--- src/builtin_cd.c
+++ src/builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: aarnell <aarnell@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/19 12:09:20 by aarnell           #+#    #+#             */
-/*   Updated: 2022/03/30 20:40:31 by aarnell          ###   ########.fr       */
+/*   Updated: 2022/04/02 20:37:41 by aarnell          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,24 +57,24 @@ static int	ft_cd_hyphen(char **path, t_exec *vars, int pos)
 	return (1);
 }
 
-static char	*ft_cd_wavy_line(char **path, t_exec *vars)
+static char	*ft_cd_wavy_line(char **path, t_exec *vars, char *dir)
 {
 	int		i;
 	char	*tmp;
 
-	i = 0;
-	tmp = NULL;
-	while (vars->envp[i])
+	i = srch_var_in_envp(vars->envp, "HOME");
+	if (i == -1)
 	{
-		tmp = ft_strnstr(vars->envp[i], "HOME=", 5);
-		if (tmp)
+		if (dir && dir[0]== '~')
 		{
 			free(*path);
-			return (ft_strdup(tmp + 5));
+			return (NULL);
 		}
-		i++;
+		return (*path);
 	}
-	return (*path);
+	tmp = get_varvalue(vars->envp[i]);
+	free(*path);
+	return (ft_strdup(tmp));
 }
 
 int	builtin_cd(char	*dir, t_exec *vars)
@@ -89,8 +89,8 @@ int	builtin_cd(char	*dir, t_exec *vars)
 	if (dir && ft_strlen(dir) == 1 && dir[0] == '-')
 		return (ft_cd_hyphen(&path, vars, pos));
 	if (!dir || dir[0] == '~')
-		path = ft_cd_wavy_line(&path, vars);
-	if (dir)
+		path = ft_cd_wavy_line(&path, vars, dir);
+	if (dir && path)
 		path = ft_cd_getpath(dir, &path);
 	if (chdir(path))
 	{
